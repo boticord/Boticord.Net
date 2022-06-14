4d6b7dd4088da7fb440d34350e3996cd606ee777
Misha133 <mihagribkov133@gmail.com>
2022-06-15 00:31:42 +0300

    add sample bot

---
+++ SampleBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using Discord.WebSocket;
+using Discord;
+using Boticord.Net;
+using Boticord.Net.Services;
+using Boticord.Net.Types;
+
+var botClient = new DiscordSocketClient();
+botClient.Ready += BotReady;
+
+var boticordClient = new BoticordClient(new BoticordConfig
+{
+    Token = "boticord token here"
+});
+
+await botClient.LoginAsync(TokenType.Bot, "bot token here");
+await botClient.StartAsync();
+
+await Task.Delay(-1);
+
+async Task BotReady()
+{
+    Console.WriteLine($"Logged into {botClient.CurrentUser}");
+
+    //run auto post things here
+}
