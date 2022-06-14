ea64a7f5cf506ef625184580d2f3d3da9aacb92e
Misha133 <mihagribkov133@gmail.com>
2022-06-14 17:16:02 +0300

    Added user info models

---
+++ Boticord.Net/Entities/UserInfo.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace Boticord.Net.Entities;
+
+public class UserInfo
+{
+    [JsonProperty("id")]
+    protected string _id { get; init; }
+
+    [JsonIgnore]
+    public ulong Id => ulong.Parse(_id);
+
+    [JsonProperty("status")]
+    public string? Status { get; init; }
+
+    [JsonProperty("shortCode")]
+    public string? ShortCode { get; init; }
+
+    [JsonProperty("badge")]
+    public string? Badge { get; init; }
+
+    [JsonProperty("site")]
+    public string? Site { get; init; }
+
+    [JsonProperty("vk")]
+    public string? VK { get; init; }
+
+    [JsonProperty("youtube")]
+    public string? Youtube { get; init; }
+
+    [JsonProperty("twitch")]
+    public string? Twitch { get; init; }
+
+    [JsonProperty("steam")]
+    public string? Steam { get; init; }
+
+    [JsonProperty("git")]
+    public string? Git { get; init; }
+
+}
