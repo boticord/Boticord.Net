423be96ce6a5d0f694579bac54be35fbe913dfe4
Misha133 <mihagribkov133@gmail.com>
2022-06-14 16:49:36 +0300

    added comment model

---
+++ Boticord.Net/Entities/Comment.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Contracts;
+using Newtonsoft.Json;
+
+namespace Boticord.Net.Entities;
+
+public class Comment
+{
+    [JsonProperty("userID")]
+    protected string _userId { get; init; }
+
+    [JsonIgnore]
+    public ulong UserId => ulong.Parse(_userId);
+
+    [JsonProperty("text")]
+    public string Text { get; init; }
+
+    [JsonProperty("vote")]
+    public int Vote { get; init; }
+
+    [JsonProperty("isUpated")]
+    public bool IsUpdated { get; init; }
+
+    [JsonProperty("createdAt")]
+    public long? CreatedAt { get; init; }
+
+    [JsonProperty("updatedAt")]
+    public long? UpdatedAt { get; init; }
+}
