4f184630d0865d31a8b304778af8e1aacf9e0be9
Misha133 <mihagribkov133@gmail.com>
2022-06-14 17:43:45 +0300

    Added some response models

---
+++ Boticord.Net/Entities/Responses/ErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Boticord.Net.Entities;
+
+public class ErrorResponse
+{
+    [JsonProperty("error")]
+    public Error Error { get; init; }
+}
+
+public class Error
+{
+    [JsonProperty("code")]
+    public string Code { get; init; }
+
+    [JsonProperty("message")]
+    public string Message { get; init; }
+}
